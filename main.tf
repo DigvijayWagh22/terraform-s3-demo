500e914f4adde92b774df523854c8d8bbe47cb50
DigvijayWagh22 <waghdigvijay2010@gmail.com>
2025-04-13 10:20:54 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@ provider "aws" {
 
 resource "aws_s3_bucket" "static_site" {
   bucket = var.bucket_name
-  acl    = "public-read"
 
   website {
     index_document = "index.html"
     error_document = "index.html"
   }
+
+  force_destroy = true
 
   tags = {
     Name = "AKMSPL Static Site"
